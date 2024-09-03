d0adf3cbdaa08002e370d0aed4ebfe83bbea058a
aungsithu-mtm <mtm.aungsithu@gmail.com>
2024-09-03 09:40:14 +0630

    jpa pratice

--- src/main/java/com/student/repository/StudentRepository.java
+++ src/main/java/com/student/repository/StudentRepository.java
@@ -2,5 +2,7 @@ package com.student.repository;
 
 import com.student.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Employee, Long> {}
